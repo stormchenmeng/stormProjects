c8b3d3092ebecb84317f7fb1504600e724b20fe7
stormchenmeng <stormchenmeng@foxmail.com>
2021-04-26 09:36:51 +0800

    CRUD

--- diy-mybatis/src/main/java/com/wind/dao/EmployeeDao.java
+++ diy-mybatis/src/main/java/com/wind/dao/EmployeeDao.java
@@ -8,7 +8,13 @@ import org.apache.ibatis.annotations.Param;
  * @Description: 这是与MyBatis中的mapper.xml文件相互绑定的接口。注意：该接口是用来操作DB的。
  */
 public interface EmployeeDao {
-    
+
+    int insertEmployee(@Param("employeeEntity") EmployeeEntity employeeEntity);
+
+    int deleteEmployee(@Param("id") int id);
+
+    int updateEmployee(@Param("employeeEntity") EmployeeEntity employeeEntity);
+
     EmployeeEntity queryEmployeeById(@Param("id") int id);
 
 }
--- diy-mybatis/src/test/java/EmployeeTest.java
+++ diy-mybatis/src/test/java/EmployeeTest.java
@@ -18,9 +18,28 @@ public class EmployeeTest {
 
         //获取实现接口的代理对象
         EmployeeDao employeeDao = sqlSession.getMapper(EmployeeDao.class);
-        EmployeeEntity employeeEntity = employeeDao.queryEmployeeById(1);
-        System.out.println(employeeEntity);
 
+        EmployeeEntity entity = new EmployeeEntity();
+        entity.setName("林青霞");
+        entity.setGender("女");
+        entity.setEmail("30@qq.com");
+        int insertEmployee = employeeDao.insertEmployee(entity);
+        System.out.println("insertEmployee=" + insertEmployee);
+
+        int deleteEmployee = employeeDao.deleteEmployee(8);
+        System.out.println("deleteEmployee =" + deleteEmployee);
+
+        entity.setId(8);
+        entity.setName("张娜拉");
+        entity.setEmail("31@qq.com");
+        int updateEmployee = employeeDao.updateEmployee(entity);
+        System.out.println("updateEmployee=" + updateEmployee);
+
+        EmployeeEntity employeeEntity = employeeDao.queryEmployeeById(1);
+        System.out.println("employeeEntity=" + employeeEntity.toString());
+
+        //提交事务
+        sqlSession.commit();
         //关闭资源
         sqlSession.close();
     }
